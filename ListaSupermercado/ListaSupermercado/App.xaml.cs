a5a5e04e368f876efa4b937951fde0edac6628fc
Usuario <Usuario@CPD03-M05>
2022-08-23 11:55:04 -0300

    Adicionar arquivos de projeto.

---
+++ ListaSupermercado/ListaSupermercado/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListaSupermercado
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage (new View.ListaProduto());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ListaSupermercado/ListaSupermercado/View/ListaProduto.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListaSupermercado.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListaProduto : ContentPage
+    {
+        public ListaProduto()
+        {
+            InitializeComponent();
+        }
+    }
+}
